97e81a736e7918d8da5537c4a377464587d9770a
morten.ege <ege.morten@gmail.com>
2018-08-29 14:46:29 +0700

    Initial commit

---
+++ ege-cards.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name:  Ege Cards
+Plugin URI:   https://github.com/mortenege/ege-cards-plugin
+Description:  Custom Created widget for SimpleFlying.com
+Version:      20180829
+Author:       Morten Ege Jensen <ege.morten@gmail.com>
+Author URI:   https://github.com/mortenege
+License:      GPLv2 <https://www.gnu.org/licenses/gpl-2.0.html>
+*/
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
